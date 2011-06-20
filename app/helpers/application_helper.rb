2b6ee203ed7449a892fb5f86fe4f99b8404ced25
Chris Held <cheld100@gmail.com>
2011-06-20 11:06:21 -0500

    fix nav sections

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 # Return a title on a per-page basis.
   def title
-    base_title = "betaArcadia"
+    base_title = "Sample App"
     if @title.nil?
       base_title
     else
