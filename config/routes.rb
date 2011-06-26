ad6ba4f301f255b35f847b48765ad7b6bb433f86
Chris Held <cheld100@gmail.com>
2011-06-26 16:06:08 -0500

    signin-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Security::Application.routes.draw do
 
+  get "sessions/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -7,8 +9,11 @@ Security::Application.routes.draw do
   get "pages/about"
   
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
